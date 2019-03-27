ec3281e8fcb07782bec6545afdcb457d81d5482a
Parvaze Uddin <robiul.islam.parvaze@gmail.com>
2019-03-27 20:45:00 +0600

    alert notification updated

--- resources/views/templates/shell.blade.php
+++ resources/views/templates/shell.blade.php
@@ -37,6 +37,16 @@
 </header>
 
 <div id="all">
+
+
+<div class="col-md-6 offset-md-3">
+<div class="alert alert-success alert-dismissible fade show" role="alert">
+  <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
+</div>
     @yield('body')
 </div>
 
