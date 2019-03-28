3232217356264ebfa719fe76301d6c484326c4d3
Parvaze Uddin <robiul.islam.parvaze@gmail.com>
2019-03-28 18:12:25 +0600

    landing page's background has been changed

--- resources/views/templates/top-bar.blade.php
+++ resources/views/templates/top-bar.blade.php
@@ -66,13 +66,8 @@
                 <span class="sr-only">Toggle navigation</span>
                 <i class="fa fa-align-justify"></i>
             </button>
-            <button type="button" data-toggle="collapse" data-target="#search" class="btn btn-outline-secondary navbar-toggler">
-                <span class="sr-only">Toggle search</span>
-                <i class="fa fa-search"></i>
-            </button>
-            <a href="basket.html" class="btn btn-outline-secondary navbar-toggler">
-                <i class="fa fa-shopping-cart"></i>
-            </a>
+           
+           
         </div>
         <div id="navigation" class="collapse navbar-collapse">
             <ul class="navbar-nav mr-auto">
@@ -83,38 +78,12 @@
         </div>
 
 
-        <div class="navbar-buttons d-flex justify-content-end">
-            <!-- /.nav-collapse-->
-            <div id="search-not-mobile" class="navbar-collapse collapse"></div>
-            <a data-toggle="collapse" href="#search" class="btn navbar-btn btn-primary d-none d-lg-inline-block">
-                <span class="sr-only">Toggle search</span>
-                <i class="fa fa-search"></i>
-            </a>
-            <div id="basket-overview" class="navbar-collapse collapse d-none d-lg-block">
-                <a href="basket.html" class="btn btn-primary navbar-btn">
-                    <i class="fa fa-shopping-cart"></i>
-                    <span>3 items in cart</span>
-                </a>
-            </div>
-        </div>
+        
 
 
     </div>
 </nav>
 
-<div id="search" class="collapse">
-    <div class="container">
-        <form role="search" class="ml-auto">
-            <div class="input-group">
-                <input type="text" placeholder="Search" class="form-control">
-                <div class="input-group-append">
-                    <button type="button" class="btn btn-primary">
-                        <i class="fa fa-search"></i>
-                    </button>
-                </div>
-            </div>
-        </form>
-    </div>
-</div>
+
 
 @yield('bulletin-ticker')
